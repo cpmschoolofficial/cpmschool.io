995bf06bbf4f26973f66330f9c88ae8c0cbe35e7
cpmschoolofficial <cpmschoolofficial@gmail.com>
2026-04-17 13:38:35 +0400

    Add menu toggle and scroll animation functions

---
+++ script.js
@@ -0,0 +1,19 @@
+function toggleMenu(){
+  document.getElementById("menu").classList.toggle("active");
+}
+
+// ANIMATION
+const elements = document.querySelectorAll('.animate');
+
+function showOnScroll(){
+  const trigger = window.innerHeight * 0.85;
+
+  elements.forEach(el=>{
+    if(el.getBoundingClientRect().top < trigger){
+      el.classList.add('show');
+    }
+  });
+}
+
+window.addEventListener('load', showOnScroll);
+window.addEventListener('scroll', showOnScroll);
