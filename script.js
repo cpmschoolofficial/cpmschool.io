8ce3933c0112da832222b9be45fa031f59c9fe38
cpmschoolofficial <cpmschoolofficial@gmail.com>
2026-04-21 08:59:07 +0400

    Fix toggleMenu function and update overlay class

--- script.js
+++ script.js
@@ -26,3 +26,9 @@ function toggleMenu(){
   document.getElementById("menu").classList.toggle("active");
   document.querySelector(".overlay").classList.toggle("active");
 }
+<script>
+function toggleMenu(){
+  document.getElementById("menu").classList.toggle("active");
+  document.querySelector(".menu-overlay").classList.toggle("active");
+}
+</script>
