31474ae3f5d4eb790f9724ea3dc3fa84640e6364
cpmschoolofficial <cpmschoolofficial@gmail.com>
2026-05-06 13:55:19 +0400

    Add slideshow functionality with next and previous controls

--- js/script.js
+++ js/script.js
@@ -78,3 +78,22 @@ function toggleMenu(){
   document.querySelector(".menu-overlay").classList.toggle("active");
 }
 </script>
+let slides = document.querySelectorAll(".slide");
+let index = 0;
+
+function showSlide(i){
+  slides.forEach(slide => slide.classList.remove("active"));
+  slides[i].classList.add("active");
+}
+
+function nextSlide(){
+  index = (index + 1) % slides.length;
+  showSlide(index);
+}
+
+function prevSlide(){
+  index = (index - 1 + slides.length) % slides.length;
+  showSlide(index);
+}
+
+setInterval(nextSlide, 4000);
